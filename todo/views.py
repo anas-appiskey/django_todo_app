43c2b94f1062e0d4e8c9b4c508e6328f46b6899e
anasali2193 <anasali2193@gmail.com>
2020-05-06 12:34:20 +0500

    Add CRUD Functionality

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import Todo
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'base.html')
+
+
+def search(request):
+    todo_items = Todo.objects.all().order_by("-added_date")
+
+    return render(request, 'todo/index.html', {
+        "todo_items" : todo_items
+    })
+
+
+def add_todo(request):
+    current_date = timezone.now()
+    current_text = request.POST["content"]
+    Todo.objects.create(added_date=current_date, text=current_text)
+    return HttpResponseRedirect("/search")
+
+
+def delete_todo(request, todo_id):
+    # print(todo_id)
+    
+    return HttpResponseRedirect("/search")
