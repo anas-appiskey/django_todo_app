4f60fef8e3f7462384b524e2abe7fed6156b9e01
anasali2193 <anasali2193@gmail.com>
2020-05-06 12:33:55 +0500

    Update urls.py

---
+++ todo/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
+    path('add_todo/', views.add_todo, name='add_todo'),
+    path('delete_todo/<int:todo_id>/', views.delete_todo, name='delete_todo'),
+
+]
+
+
